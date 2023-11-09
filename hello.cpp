95a873b57037d076986f9a2a3f06412f59b59918
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-11-09 18:54:57 -0300

    first commit

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+// assim posso omitir o std na função
+using namespace std;
+
+int main()
+{
+  // std = função padrão de saída
+  // cout = função de saída
+  // endl = quebra de linha
+
+  std::cout << "Hello World!" << std::endl;
+
+  // pausa a tela do executável
+  system("pause");
+
+  return 0;
+}
