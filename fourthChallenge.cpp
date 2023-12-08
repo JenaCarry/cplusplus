b97980770b90a8503f7901a8ddbff60f869a34ff
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-12-08 20:33:37 -0300

    Fourth Challenge

--- fourthChallenge.cpp
+++ fourthChallenge.cpp
@@ -2,14 +2,47 @@
 
 using namespace std;
 
+float pow(int base, int exp)
+{
+  float result = base;
+
+  for (int i = 1; i < exp; i++)
+  {
+    result *= base;
+  }
+
+  return result;
+}
+
 int main()
 {
   int base, exp;
-  cout << "Vamos calcular a potencia \n Informe um numero real para a base: ";
+  cout << "Vamos calcular a potencia! \nDigite um numero real para a Base (a): ";
   cin >> base;
 
-  cout << "E agora informe numero para o expoente: ";
+  cout << "Digite um numero para o Expoente (n): ";
   cin >> exp;
+
+  cout << "O resultado da potencia de " << base << " elevado a " << exp << " e igual a " << pow(base, exp) << endl;
+
+  char key = 's';
+
+  while (key == 's')
+  {
+    cout << "Voce gostaria de calcular outra potencia? Digite S ou N: ";
+    cin >> key;
+
+    if (key == 's')
+    {
+      cout << "Digite um numero real para a Base (a): ";
+      cin >> base;
+
+      cout << "Digite um numero para o Expoente (n): ";
+      cin >> exp;
+
+      cout << "O resultado da potencia de " << base << " elevado a " << exp << " e igual a " << pow(base, exp) << endl;
+    }
+  }
 
   return 0;
 }
