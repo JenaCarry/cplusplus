8f81de62dbd90afd750c27618e7a4131068fd04e
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-12-08 19:29:41 -0300

    Third Challenge

---
+++ thirdChallenge.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int factorialWhile(int num)
+{
+  int count = 1;
+  int result = 1;
+
+  while (count <= num)
+  {
+    result *= count;
+    count++;
+  }
+
+  return result;
+}
+
+int factorialFor(int num)
+{
+  int result = 1;
+
+  for (int count = 1; count <= num; count++)
+  {
+    result *= count;
+  }
+
+  return result;
+}
+
+int main()
+{
+  int fact;
+
+  cout << "Informe o fatorial desejado: ";
+  cin >> fact;
+
+  int resultWhile = factorialWhile(abs(fact));
+  int resultFor = factorialFor(abs(fact));
+
+  cout << "O fatorial de " << abs(fact) << "! e a = " << resultWhile << endl;
+  cout << "O fatorial de " << abs(fact) << "! e a = " << resultFor;
+
+  return 0;
+}
