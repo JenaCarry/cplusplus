e44e273f1616154f331570b5056f25de227cda88
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-12-08 19:39:54 -0300

    Forth Challenge

--- fourthChallenge.cpp
+++ fourthChallenge.cpp
@@ -1,1 +1,15 @@
 #include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int base, exp;
+  cout << "Vamos calcular a potencia \n Informe um numero real para a base: ";
+  cin >> base;
+
+  cout << "E agora informe numero para o expoente: ";
+  cin >> exp;
+
+  return 0;
+}
